2beaa34209c0d2e2c373a9eae7130edca8c256dc
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-21 19:53:59 -0400

    test: sad path, email taken and email blank

--- spec/requests/api/v1/registration_spec.rb
+++ spec/requests/api/v1/registration_spec.rb
@@ -52,5 +52,50 @@ RSpec.describe "User Registration", type: :request do
       expect(registration_response[:errors].first).to be_a(Hash)
       expect(registration_response[:errors].first[:detail]).to eq("Validation failed: Password confirmation doesn't match Password, Password confirmation doesn't match Password")
     end
+
+    it 'raises 400 error if email is already taken' do
+      user_1 = create(:user, email: 'user@example.com', password: 'password', password_confirmation: 'password')
+
+      user_params = {
+        email: 'user@example.com',
+        password: 'test',
+        password_confirmation: 'test'
+      }
+
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user: user_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      registration_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(registration_response).to have_key(:errors)
+      expect(registration_response[:errors]).to be_an(Array)
+
+      expect(registration_response[:errors].first).to be_a(Hash)
+      expect(registration_response[:errors].first[:detail]).to eq("Validation failed: Email has already been taken")
+    end
+
+    it 'raises 400 error if field is missing' do
+      user_1 = create(:user, email: 'user@example.com', password: 'password', password_confirmation: 'password')
+
+      user_params = {
+        password: 'test',
+        password_confirmation: 'test'
+      }
+
+      post "/api/v1/users", headers: @headers, params: JSON.generate(user: user_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      registration_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(registration_response).to have_key(:errors)
+      expect(registration_response[:errors]).to be_an(Array)
+
+      expect(registration_response[:errors].first).to be_a(Hash)
+      expect(registration_response[:errors].first[:detail]).to eq("Validation failed: Email can't be blank")
+    end
   end
 end
