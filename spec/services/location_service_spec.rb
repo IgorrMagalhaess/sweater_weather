e9a12650242caecb01b3a5aa27347e39532a9aa7
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-23 15:16:54 -0400

    test location service get_route_info

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe LocationService do
       expect(coordinates[:results].first[:locations].first[:latLng].values).to eq([33.74831, -84.39111])
     end
   end
+
+  describe "get_route_info" do
+    it 'returns route information for the given start and end cities', :vcr do
+      route_info = LocationService.new.get_route_info("New York", "Los Angeles")
+
+      expect(route_info).to be_a(Hash)
+      expect(route_info).to have_key(:route)
+      expect(route_info[:route]).to have_key(:formattedTime)
+      expect(route_info[:route][:formattedTime]).to be_a(String)
+    end
+  end
 end
